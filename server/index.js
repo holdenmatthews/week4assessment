6dc363c27ab6282729eeb2b244d184b9485aa7b0
Holden Matthews <holdenmatthews1@gmail.com>
2023-01-26 16:02:47 -0700

    deploying

--- server/index.js
+++ server/index.js
@@ -7,6 +7,8 @@ app.use(cors());
 
 app.use(express.json());
 
+app.use(express.static(`${__dirname}/client`))
+
 const { getCompliment, getFortune, getColor, getName, addFood, deleteFood, editFood } = require('./controller')
 
 app.get("/api/compliment", getCompliment);
