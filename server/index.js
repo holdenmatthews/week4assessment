7b71c9d8a7e5789306105147674aff568198fc6c
Holden Matthews <holdenmatthews1@gmail.com>
2023-01-26 16:38:55 -0700

    rollbar added

--- server/index.js
+++ server/index.js
@@ -9,6 +9,17 @@ app.use(express.json());
 
 app.use(express.static(`${__dirname}/../client`))
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '05713790d105435a958f62a968079725',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
 const { getCompliment, getFortune, getColor, getName, addFood, deleteFood, editFood } = require('./controller')
 
 app.get("/api/compliment", getCompliment);
